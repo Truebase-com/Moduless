946430877648e53a478ef795361395455982198d
paulgo <paul@truebase.com>
2019-10-22 13:33:14 -0400

    Assigns a terminal tab title on run

--- source/Moduless.ts
+++ source/Moduless.ts
@@ -204,11 +204,19 @@ function launchServer(
 		console.log("Server listening on port: " + options.port);
 }
 
-/**
- * 
- */
+/** */
+function setTerminalTitle(title: string)
+{
+	process.stdout.write(
+		String.fromCharCode(27) + "]0;" + title + String.fromCharCode(7)
+	);
+}
+
+/** */
 (function start()
 {
+	setTerminalTitle("Moduless");
+	
 	const outFiles = findNestedOutFiles(process.cwd());
 	const commonPath = findCommonPath(outFiles);
 	const outFilesRelative = outFiles.map(p => p.slice(commonPath.length));
